dc13d3039613fe543b38354a28f7803c4a3705e0
y-salnikov <y.salnikov65535@gmail.com>
2016-08-22 23:28:45 +0400

    update

--- sound/psg_player/ay.c
+++ sound/psg_player/ay.c
@@ -1,2 +1,31 @@
+#include <stdlib.h>
 #include "types.h"
 
+#define CLK 1750000 // Hz
+
+
+ay_context* ay_init(int samplerate)
+{
+	uint8_t i;
+	ay_context *ayc;
+	ayc=malloc(sizeof(ay_context));
+	if(ayc==NULL) return NULL;
+	for(i=0;i<16;i++) ayc->regs[i]=0;
+	ayc->tone_wave[0]=ayc->tone_wave[1]=ayc->tone_wave[2]=0;
+	ayc->tone_counters[0]=ayc->tone_counters[1]=ayc->tone_counters[2]=0;
+	auc->envelope_counter=0;
+	ayc->envelope=0;
+	ayc->tick_fraction=0.0;
+	ayc->tps=(((float)CLK)/samplerate)/16.0;
+	return ayc;
+}
+
+void ay_get_sample(ay_context *ayc)
+{
+	
+}
+
+void ay_done(ay_context *ayc)
+{
+	free(ayc);
+}
--- sound/psg_player/types.h
+++ sound/psg_player/types.h
@@ -3,5 +3,10 @@ typedef struct ay_struct
 {
 	uint8_t regs[16];
 	uint8_t tone_wave[3];
+	uint16_t tone_counters[3];
+	uint16_t noise_counter;
+	uint16_t envelope_counter;
 	uint8_t envelope;
+	float 	tick_fraction;
+	float 	tps;			// ticks per sample
 } ay_context;
